c3c9353e345e242a837d1e88ac50706b3d7e3c49
mpaulazzv <mariapaulaflorezvargas11@gmail.com>
2023-10-24 15:08:01 -0500

    Problema filósosfos solucionado

--- filosofos.py
+++ filosofos.py
@@ -1,5 +1,7 @@
 import threading 
+from multiprocessing import Process
 import multiprocessing
+import time
 
 num_filosofos = 5
 disponible = 1
@@ -7,12 +9,14 @@ no_disponible = 0
 
 tenedores = []
 estado = []
+for i in range(num_filosofos):
+        estado.append(disponible)
 filosofos = []
 
 def comensales (args):
     
-    print("filósofo["+str(args)+"] pensando ...")
-    print("filósofo["+str(args)+"] hambriento ...")
+    print("filósofo["+str(args+1)+"] pensando ...")
+    print("filósofo["+str(args+1)+"] hambriento ...")
 
     if (args == 0):
         a = num_filosofos - 1
@@ -20,12 +24,13 @@ def comensales (args):
         a = args - 1
     
     if (estado[args] == disponible and estado[a]==disponible):
-        tenedores[args].acquire(blocking=True)
-        tenedores[a].acquire(blocking=True)
+        tenedores[args].acquire()
+        tenedores[a].acquire()
         estado[args] = no_disponible
         estado[a] = no_disponible
         print("El filósofo["+str(args+1)+"] toma los tenedores (" + str(args+1)+", "+str(a+1)+")")
         print("Filósofo ["+str(args+1)+"] comiendo...")
+        
     
     if(estado[args] == no_disponible and estado[a]==no_disponible):
         tenedores[args].release()
@@ -33,60 +38,32 @@ def comensales (args):
         estado[args] = disponible
         estado[a] = disponible
         print("El filósofo ["+str(args+1)+"] entrega los tenedores ("+ str(args+1)+", "+str(a+1)+")")
+    
+    time.sleep(2)
 
-i = 0
 
-for i in range(i < num_filosofos):
-    estado[i] = disponible
+if __name__ == "__main__":
+    c = 0
+    i =0
+    for i in range(num_filosofos):
+        estado[i] = disponible
+
+    for i in range (num_filosofos):
+        tenedores.append(multiprocessing.Semaphore(1))
+
+    for i in range (num_filosofos):
+        filosofos.append(Process(target=comensales, args=[i]))
+        filosofos[i].start()
 
 while True:
 
     for i in range (num_filosofos):
-        tenedores[i] = multiprocessing.Semaphore()
+        tenedores[i] = multiprocessing.Semaphore(1)
 
     for i in range (num_filosofos):
-        filosofos[i] = threading.Thread(target=comensales, args=(i))
-    
+        filosofos[i] = (Process(target=comensales, args=[i]))
+        filosofos[i].start()
+
     for i in range (num_filosofos):
         filosofos[i].join()
     
-    for i in range (num_filosofos):
-        
-
-    
-
-
-#     void main(int argc, char *argv[])
-# {
-#     int i;
-#     for (i = 0; i < NUM_FILOSOFO; i++)
-#     {
-#         estado[i] = DISPONIBLE;
-#     }
-
-#     while (1)
-#     {
-
-#         pthread_t filosofos[NUM_FILOSOFO];
-
-#         for (i = 0; i < NUM_FILOSOFO; i++)
-#         {
-#             sem_init(&tenedores[i], 0, 1);
-#         }
-
-#         for (i = 0; i < NUM_FILOSOFO; i++)
-#         {
-#             pthread_create(&filosofos[i], NULL, &comensales, &i);
-#         }
-
-#         for (i = 0; i < NUM_FILOSOFO; i++)
-#         {
-#             pthread_join(filosofos[i], NULL);
-#         }
-
-#         for (i = 0; i < NUM_FILOSOFO; i++)
-#         {
-#             sem_destroy(&tenedores[i]);
-#         }
-#     }
-# }
