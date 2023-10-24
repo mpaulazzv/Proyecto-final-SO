5452aef2461d5327cb928c9dd588d35270f9693e
Pablo1923 <pablo@DESKTOP-BQ6VL1N.localdomain>
2023-10-24 14:12:50 -0500

    new changes

--- filosofos.py
+++ filosofos.py
@@ -1,4 +1,5 @@
 import threading 
+import multiprocessing
 
 num_filosofos = 5
 disponible = 1
@@ -33,3 +34,59 @@ def comensales (args):
         estado[a] = disponible
         print("El filósofo ["+str(args+1)+"] entrega los tenedores ("+ str(args+1)+", "+str(a+1)+")")
 
+i = 0
+
+for i in range(i < num_filosofos):
+    estado[i] = disponible
+
+while True:
+
+    for i in range (num_filosofos):
+        tenedores[i] = multiprocessing.Semaphore()
+
+    for i in range (num_filosofos):
+        filosofos[i] = threading.Thread(target=comensales, args=(i))
+    
+    for i in range (num_filosofos):
+        filosofos[i].join()
+    
+    for i in range (num_filosofos):
+        
+
+    
+
+
+#     void main(int argc, char *argv[])
+# {
+#     int i;
+#     for (i = 0; i < NUM_FILOSOFO; i++)
+#     {
+#         estado[i] = DISPONIBLE;
+#     }
+
+#     while (1)
+#     {
+
+#         pthread_t filosofos[NUM_FILOSOFO];
+
+#         for (i = 0; i < NUM_FILOSOFO; i++)
+#         {
+#             sem_init(&tenedores[i], 0, 1);
+#         }
+
+#         for (i = 0; i < NUM_FILOSOFO; i++)
+#         {
+#             pthread_create(&filosofos[i], NULL, &comensales, &i);
+#         }
+
+#         for (i = 0; i < NUM_FILOSOFO; i++)
+#         {
+#             pthread_join(filosofos[i], NULL);
+#         }
+
+#         for (i = 0; i < NUM_FILOSOFO; i++)
+#         {
+#             sem_destroy(&tenedores[i]);
+#         }
+#     }
+# }
